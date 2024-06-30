110463adf357212f771402447ec7f23fafa4c891
keinuma <nununu.mono@gmail.com>
2024-06-30 16:33:40 +0900

    generate route to get bookmarks

---
+++ src/app/api/bookmarks/route.ts
@@ -0,0 +1,12 @@
+// write a route to get all bookmarks by Next.js
+// bookmarks has a title and URL
+
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const getAllBookmarks = (req: NextApiRequest, res: NextApiResponse) => {
+  // Your code to get all bookmarks goes here
+};
+
+export default getAllBookmarks;
+
+
